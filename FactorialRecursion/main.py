8e0d48cc9215b7bff7e12d5f801af0fe134d5292
Dani <datik2012@gmail.com>
2026-03-29 19:25:46 +0300

    Factorial - Recursion method

---
+++ FactorialRecursion/main.py
@@ -0,0 +1,26 @@
+
+
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+   
+  
+
+    number = int(input("Enter a number to calculate its factorial: "))
+    result = factorial(number)
+    print(f"The factorial of {number} is {result}.")
+    
+    factorial(number)
